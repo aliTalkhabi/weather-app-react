6fe0622e11ae2e9ab3e93b563aaf47e88a1aba46
aliTalkhabi <atdes1999@gmail.com>
2023-06-21 16:55:19 +0330

    fix error and not use forecast component

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { iconUrlFromCode } from "./services/weatherServices";
 
-function Forecast({title, }) {
+function Forecast({ title, items }) {
+  console.log(items);
   return (
     <>
       <div className="flex items-center justify-start mt-6">
@@ -8,46 +10,17 @@ function Forecast({title, }) {
       </div>
       <hr className="my-2" />
       <div className="flex flex-row items-center justify-between text-white">
-        <div className="flex flex-col items-center justify-center">
-          <p className="font-light text-sm">04:30 PM</p>
-          <img
-            src="https://openweathermap.org/img/wn/01d@2x.png"
-            className="w-12 my-1"
-          />
-          <p className="font-medium">22°</p>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <p className="font-light text-sm">04:30 PM</p>
-          <img
-            src="https://openweathermap.org/img/wn/01d@2x.png"
-            className="w-12 my-1"
-          />
-          <p className="font-medium">22°</p>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <p className="font-light text-sm">04:30 PM</p>
-          <img
-            src="https://openweathermap.org/img/wn/01d@2x.png"
-            className="w-12 my-1"
-          />
-          <p className="font-medium">22°</p>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <p className="font-light text-sm">04:30 PM</p>
-          <img
-            src="https://openweathermap.org/img/wn/01d@2x.png"
-            className="w-12 my-1"
-          />
-          <p className="font-medium">22°</p>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <p className="font-light text-sm">04:30 PM</p>
-          <img
-            src="https://openweathermap.org/img/wn/01d@2x.png"
-            className="w-12 my-1"
-          />
-          <p className="font-medium">22°</p>
-        </div>
+        {items.map((item) => (
+          <div className="flex flex-col items-center justify-center">
+            <p className="font-light text-sm">{item.title}</p>
+            <img
+              src={iconUrlFromCode(item.icon)}
+              alt="icon-1"
+              className="w-12 my-1"
+            />
+            <p className="font-medium">{`${item.temp.toFixed()}°`}</p>
+          </div>
+        ))}
       </div>
     </>
   );
